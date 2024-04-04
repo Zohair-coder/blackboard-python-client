ecb6135fe3a7185a0c9545edcd312a97079f397d
Zohair-coder <zohair.ul.hasan@gmail.com>
2024-04-03 20:03:07 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from bbrest import BbRest
+
+import os
+import json
+
+key = os.environ["BLACKBOARD_APP_KEY"]
+secret = os.environ["BLACKBOARD_SECRET"]
+url = os.environ["BLACKBOARD_URL"]
+
+bb = BbRest(key, secret, url, verify=False)
+
+print(bb.expiration())
+
+courses = bb.GetCourses().json()["results"]
+print(json.dumps(courses, indent=2))
+
+# print(json.dumps(list(bb.functions.keys()), indent=2))
+
